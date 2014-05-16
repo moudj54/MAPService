7b4c89fde7d9469116c5ac541c7ee9fcc7dd59ab
pminervini <p.minervini@gmail.com>
2014-05-16 09:36:36 +0200

    update

--- test/main/java/com/neuralnoise/map/service/test/GeoLocationServiceTests.java
+++ test/main/java/com/neuralnoise/map/service/test/GeoLocationServiceTests.java
@@ -7,6 +7,8 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -18,11 +20,15 @@ import com.neuralnoise.map.service.geo.GeoLocationService;
 @ContextConfiguration("classpath:spring/web-context.xml")
 public class GeoLocationServiceTests {
 
+	private static final Logger log = LoggerFactory.getLogger(GeoLocationServiceTests.class);
+	
 	@Autowired
 	private GeoLocationService geoService;
 
 	@Test
 	public void testSayHello() throws IOException {
+		
+		log.info("Looking up 'Bari' using Google ..");
 		List<Location> bari = geoService.lookupGoogle("Bari");
 		//Assert.assertEquals("Hello world!", helloService.sayHello());
 	}
